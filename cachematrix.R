32dbdc115e7cefaccc247768f9ddfea23813e2f8
Elena <elena.nur88@gmail.com>
2025-05-16 14:59:00 +0200

    task inverted matrix cache

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        im <- NULL
+        set <- function(y) {
+                x <<- y
+                im <<- NULL
+        }
+        get <- function() x
+        setinversed <- function(inversed) im <<- inversed
+        getinversed <- function() im
+        list(set=set, get=get, setinversed=setinversed, getinversed=getinversed)
 }
 
 
@@ -12,4 +20,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        im <- x$getinversed()
+        if (!is.null(im)){
+                message("getting cache data")
+                im
+        }
+        data <- x$get()
+        im <- solve(data)
+        x$setinversed(im)
+        im
 }
